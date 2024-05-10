b4df1f782fab02ab25fdf2e6bb41a89988752428
Cherem2002 <Cherem2002@gmail.com>
2024-05-10 15:42:24 +0800

    Добавил basename

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import Home from './pages/home/home';
-import Gallary from './pages/gallary/gallary';
-import Impr from './pages/impr/Impr';
-import Cl from './pages/cl/cl';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home/home';
+import Gallery from './pages/gallary/gallary';
 import Modern from './pages/modern/modern';
+import Impr from './pages/impr/Impr';
+import Cl from './pages/cl/cl';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/gallary" element={<Gallary />} />
-          <Route path="/gallary/modern" element={<Modern />} />
-          <Route path="/gallary/impr" element={<Impr />} />
-          <Route path="/gallary/cl" element={<Cl />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/gallery/modern" element={<Modern />} />
+          <Route path="/gallery/impr" element={<Impr />} />
+          <Route path="/gallery/cl" element={<Cl />} />
         </Routes>
       </div>
     </Router>
-
   );
 }
 
 export default App;
+
